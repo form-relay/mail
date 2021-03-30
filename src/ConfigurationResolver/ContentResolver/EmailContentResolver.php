066d1440843ba8a9890b8163748d960704814760
Stephan Ude <ude@mediatis.de>
2021-03-30 21:55:01 +0200

    LMSCB-16: refactored configuration resolvers (form-relay/core)

--- src/ConfigurationResolver/ContentResolver/EmailContentResolver.php
+++ src/ConfigurationResolver/ContentResolver/EmailContentResolver.php
@@ -8,16 +8,32 @@ use FormRelay\Mail\Model\Form\EmailField;
 class EmailContentResolver extends ContentResolver
 {
     const KEY_ADDRESS = 'address';
+    const DEFAULT_ADDRESS = '';
+
     const KEY_NAME = 'name';
+    const DEFAULT_NAME = '';
 
     public function build()
     {
-        $config = is_array($this->config) ? $this->config : [static::KEY_ADDRESS => $this->config];
-        $name = isset($config[static::KEY_NAME]) ? trim($this->resolveContent($config[static::KEY_NAME])) : '';
-        $address = isset($config[static::KEY_ADDRESS]) ? trim($this->resolveContent($config[static::KEY_ADDRESS])) : '';
+        if (!is_array($this->configuration)) {
+            $this->configuration = [static::KEY_ADDRESS => $this->configuration];
+        }
+
+        $name = trim($this->resolveContent($this->getConfig(static::KEY_NAME)));
+        $address = trim($this->resolveContent($this->getConfig(static::KEY_ADDRESS)));
+
         if (!$address) {
             return null;
         }
+
         return new EmailField($address, $name);
+    }
+
+    public static function getDefaultConfiguration(): array
+    {
+        return parent::getDefaultConfiguration() + [
+            static::KEY_ADDRESS => static::DEFAULT_ADDRESS,
+            static::KEY_NAME => static::DEFAULT_NAME,
+        ];
     }
 }
